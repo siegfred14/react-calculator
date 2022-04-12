08945f1dba8e05f522248a969d4746c6c1a54f45
Siegfred Samson <siegfredsamson@gmail.com>
2022-04-12 15:54:51 +0100

    initial styles.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from "./logo.svg";
-import "./App.css";
+import "./styles.css";
 import Calculator from "./components/Calculator";
 
 function App() {
   return (
-    <div className="App">
+    <div className="calculator-grid">
       <Calculator />
     </div>
   );
