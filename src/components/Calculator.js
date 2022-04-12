599f95de91cac9405f69581c4871aff40ac989ed
Siegfred Samson <siegfredsamson@gmail.com>
2022-04-12 15:40:31 +0100

    Output section created

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -4,6 +4,10 @@ export default function Calculator() {
   return (
     <div>
       <h1>Null</h1>
+      <div className="output">
+        <div className="previous-operand"></div>
+        <div className="current-operand"></div>
+      </div>
     </div>
   );
 }
