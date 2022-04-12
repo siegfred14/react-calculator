4fe4b47381ce234c61622c57416042e57cd9036a
Siegfred Samson <siegfredsamson@gmail.com>
2022-04-12 16:44:17 +0100

    calculator grid and flex designed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import Calculator from "./components/Calculator";
 
 function App() {
   return (
-    <div className="calculator-grid">
+    <div className="calculator">
+      <h1>Simple Calculator</h1>
       <Calculator />
     </div>
   );
