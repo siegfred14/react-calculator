547d716dcb377b7f28e82f957573657c99de0372
Siegfred Samson <siegfredsamson@gmail.com>
2022-04-15 09:40:27 +0100

    regular buttons replaced with DigitButton and OperationButton

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,6 +1,7 @@
 import React, { useReducer } from "react";
+import DigitButton from "./DigitButton";
 
-const ACTIONS = {
+export const ACTIONS = {
   ADD_DIGIT: "add-digit",
   CHOOSE_OPERATION: "choose-operation",
   CLEAR: "clear",
@@ -25,7 +26,7 @@ export default function Calculator() {
     {}
   );
 
-  dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 1 } });
+  //   dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 1 } });
 
   return (
     <div className="calculator-grid">
@@ -37,25 +38,27 @@ export default function Calculator() {
       </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
-      <button>/</button>
+      <OperationButton operation="/" dispatch={dispatch} />
 
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>*</button>
+      <DigitButton digit="1" dispatch={dispatch} />
+      <DigitButton digit="2" dispatch={dispatch} />
+      <DigitButton digit="3" dispatch={dispatch} />
 
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>+</button>
+      <OperationButton operation="*" dispatch={dispatch} />
 
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>-</button>
+      <DigitButton digit="4" dispatch={dispatch} />
+      <DigitButton digit="5" dispatch={dispatch} />
+      <DigitButton digit="6" dispatch={dispatch} />
+      <OperationButton operation="+" dispatch={dispatch} />
 
-      <button>.</button>
-      <button>0</button>
+      <DigitButton digit="7" dispatch={dispatch} />
+      <DigitButton digit="8" dispatch={dispatch} />
+      <DigitButton digit="9" dispatch={dispatch} />
+      <OperationButton operation="-" dispatch={dispatch} />
+
+      <DigitButton digit="0" dispatch={dispatch} />
+      <DigitButton digit="." dispatch={dispatch} />
+      {/* <OperationButton operation="+" dispatch={dispatch} /> */}
       <button className="span-two">=</button>
     </div>
   );
