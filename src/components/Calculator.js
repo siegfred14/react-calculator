a1cf8b822d25eaca678647f33e7372f9419d8f39
Siegfred Samson <siegfredsamson@gmail.com>
2022-04-16 10:09:54 +0100

    keeping initial 0 to 1 digit

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -13,6 +13,7 @@ export const ACTIONS = {
 const reducer = (state, { type, payload }) => {
   switch (type) {
     case ACTIONS.ADD_DIGIT:
+      if (payload.digit === "0" && state.currentOperand === "0") return state;
       return {
         ...state,
         currentOperand: `${state.currentOperand || ""}${payload.digit}`,
@@ -57,8 +58,8 @@ export default function Calculator() {
       <DigitButton digit="9" dispatch={dispatch} />
       <OperationButton operation="-" dispatch={dispatch} />
 
-      <DigitButton digit="0" dispatch={dispatch} />
       <DigitButton digit="." dispatch={dispatch} />
+      <DigitButton digit="0" dispatch={dispatch} />
       {/* <OperationButton operation="+" dispatch={dispatch} /> */}
       <button className="span-two">=</button>
     </div>
